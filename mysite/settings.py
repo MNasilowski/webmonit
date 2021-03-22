37d79936d2b8f920d0430650de95ee8378a4632d
mnasilowski <nasilowski.marcin@gmail.com>
2021-03-22 08:49:12 +0100

    removed one line

--- mysite/settings.py
+++ mysite/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'suy%$rnj%2!^0nj$q3dig*=l_sjv1r_9=kgb2m#t_haj80c@^&'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
